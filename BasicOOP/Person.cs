42e84a59754636e5081f5236f257bf2e81413b4f
Jonathan Krall Olsson <jonathan.krall@lexicon.se>
2024-03-05 13:45:39 +0100

    Added Person base-class and Derived classes

---
+++ BasicOOP/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicOOP
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+
+        public virtual void Do()
+        {
+            Console.WriteLine("Person do");
+        }
+
+        //Finns alltid med
+        //public Person()
+        //{
+
+        //}
+        public Person(string name)
+        {
+            Name = name;
+        }
+    }
+
+    internal class Employee : Person
+    {
+        public int Salary { get; set; }
+        public Employee(string name, int salary) : base(name)
+        {
+            Salary = salary;
+        }
+
+    }
+
+    internal class Admin : Employee
+    {
+        public string Department { get; set; }
+        public Admin(string name, int salary, string department) : base(name, salary)
+        {
+            Department = department; 
+        }
+
+        public override void Do()
+        {
+            //base.Do();
+            Console.WriteLine("Admin do");
+        }
+    }
+}
--- BasicOOP/Program.cs
+++ BasicOOP/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
+            Person p = new Person("Kalle");
+            Employee e = new Employee("Anna", 25000);
+            Admin a = new Admin("Stefan", 50000, "HR");
 
+            Console.WriteLine(p.Name);
+            Console.WriteLine(e.Name + " " + e.Salary);
+            Console.WriteLine($"{a.Name} {a.Salary} {a.Department}");
+            p.Do();
+            e.Do();
+            a.Do(); 
+
+
+        
         }
 
 
